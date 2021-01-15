ee355fec74821c22d44fac2026159aa38b5db5cc
antonio-cajueiro-campos <kimeesan5@gmail.com>
2021-01-15 12:10:31 -0300

    criação dos metodos e da estrutura de funcionalidade de salvar piadas

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,14 @@
           </div>
           <div class="line"></div>
           <input type="submit" class="btn btn-primary input-group-btn nextJk" value="Próxima piada!">
+          <!--<button type="button" class="btn btn-outline-primary input-group-btn saveJk" @click="saveJoke()">Salvar piada!</button>
+          <button v-if="saved.length != 0 && savedOn == false" type="button" class="btn btn-outline-primary input-group-btn savedJk" @click="savedList()">Mostrar piadas salvas</button>
+          <button v-if="saved.length != 0 && savedOn" type="button" class="btn btn-outline-primary input-group-btn savedJk" @click="savedList()">Ocultar piadas salvas</button> -->
         </div>
       </form>
+      <!-- <div class="saved">
+        <PiadasSalvas v-if="savedOn" />
+      </div> -->
     </div>
     <footer>
       <p>Antonio Carlos - <a href="https://github.com/antonio-cajueiro-campos">GitHub</a> | Powered By <a target="_blank" href="https://icanhazdadjoke.com">icanhazdadjoke.com</a></p>
@@ -25,14 +31,28 @@
 </template>
 
 <script>
+//import PiadasSalvas from "./components/piadasSalvas";
 export default {
   name: 'App',
+  //components: { PiadasSalvas },
   data() {
-    return {piada: "", json: null};
+    return {piada: "", json: null, saved: [], savedOn: false};
   },
   methods: {
+    savedList() {
+      
+    },
 
-    // método para obter o json da api através
+    //salva a piada num array para exibir posteriormente em um componente
+    saveJoke() {
+     
+      this.saved.push(this.piada)
+      this.saved = this.saved.filter(function(elem, index, self) {
+        return index === self.indexOf(elem);
+      });
+    },
+
+    // método para obter o json da api através do fetch API
     async getJson() {
       const url = `https://icanhazdadjoke.com/slack`;
       this.json = await fetch(url).then(response => response.json());
@@ -40,11 +60,10 @@ export default {
   
     // método para mostrar a piada
     async show() {
-      var newJoke;
+      let newJoke;
 
       // verifica se o json recebido não está no estado inicial de nulo
       if (this.json != null) {
-
         newJoke = this.json.attachments[0].fallback;
 
         // verifica se o json recebido é igual ao anterior para evitar que piada se reptia duas vezes consecutivas
@@ -86,13 +105,23 @@ export default {
 header {
   background: rgb(206, 206, 206);
 }
-.initBtn, .nextJk {
+.initBtn, .nextJk, .saveJk, .savedJk {
   position: absolute;
   top: 300px;
   left: 50%;
   transform: translate(-50%);
 }
+.nextJk {
+  left: 42%;
+}
+.saveJk {
+  left: 58%;
+}
+.savedJk {
+  top: 410px;
+}
 footer {
+  z-index: 5;
   position: absolute;
   bottom: 0;
   background: rgb(199, 199, 199);
--- src/components/piadasSalvas.vue
+++ src/components/piadasSalvas.vue
@@ -1,13 +1,27 @@
 <template>
-  
+	<div class=lista>
+		
+	</div>
 </template>
 
 <script>
 export default {
-
+	props: {
+		saved: {
+			type: Array, required: false
+		}
+	}
 }
 </script>
 
 <style>
-
+.lista {
+	height: 100%;
+	width: 400px;
+	background: rgb(199, 199, 199);
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: -1;
+}
 </style>
